341fd0745856764584d7bbfea4fe06de2e50c337
dreamjbj1 <999888@naver.com>
2023-07-04 17:22:21 +0900

    20230704..

---
+++ test2Project/music.api/music.js
@@ -0,0 +1,35 @@
+let allMusic = [
+    {
+        name : "Joker (Feat. JAMIE)",
+        artist : "BIG Naughty(서동현)",
+        img : "banner1",
+    },
+    {
+        name : "내 기쁨은 너가 벤틀리를 끄는 거야",
+        artist : "김승민",
+        img : "banner2"        
+    },
+    {
+        name : "Misfits (Feat. MELOH, Kid Wine)",
+        artist : "Skinny Brown",
+        img : "banner3"        
+    },
+    {
+        name : "검정색하트 (Feat. Leellamarz, BE'O)",
+        artist : "TOIL",
+        img : "banner4",
+        audio : "music-4"
+    },
+    {
+        name : "우리서로사랑하지는말자",
+        artist : "기리보이",
+        img : "banner5"
+        
+    },
+    {
+        name : "BROKEN FERRARI",
+        artist : "호미들",
+        img : "banner6",
+        
+    },
+]
